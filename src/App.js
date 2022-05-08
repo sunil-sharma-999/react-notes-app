44a8ab0921686daf80b57e662bbdd51d99f38eab
sunil-sharma-999 <84917190+sunil-sharma-999@users.noreply.github.com>
2022-05-08 23:10:29 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import React, { useEffect } from 'react';
+import Sidebar from './components/Sidebar';
+import Editor from './components/Editor';
+import Split from 'react-split';
+import { nanoid } from 'nanoid';
+
+export default function App() {
+  const [notes, setNotes] = React.useState(
+    () => JSON.parse(localStorage.getItem('notes')) || [],
+  );
+
+  useEffect(() => {
+    localStorage.setItem('notes', JSON.stringify(notes));
+  }, [notes]);
+
+  const [currentNoteId, setCurrentNoteId] = React.useState(
+    (notes[0] && notes[0].id) || '',
+  );
+
+  function createNewNote() {
+    const newNote = {
+      id: nanoid(),
+      body: "# Type your markdown note's title here",
+    };
+    setNotes((prevNotes) => [newNote, ...prevNotes]);
+    setCurrentNoteId(newNote.id);
+  }
+
+  function updateNote(text) {
+    setNotes((oldNotes) => {
+      const newArr = [];
+      for (let oldNote of oldNotes) {
+        oldNote.id === currentNoteId
+          ? newArr.unshift({ ...oldNote, body: text })
+          : newArr.push(oldNote);
+      }
+      return newArr;
+    });
+  }
+
+  function findCurrentNote() {
+    return (
+      notes.find((note) => {
+        return note.id === currentNoteId;
+      }) || notes[0]
+    );
+  }
+
+  function deleteNote(e, id) {
+    e.stopPropagation();
+    setNotes((oldNotes) => oldNotes.filter((note) => note.id !== id));
+  }
+
+  return (
+    <main>
+      {notes.length > 0 ? (
+        <Split sizes={[30, 70]} direction="horizontal" className="split">
+          <Sidebar
+            notes={notes}
+            currentNote={findCurrentNote()}
+            setCurrentNoteId={setCurrentNoteId}
+            newNote={createNewNote}
+            deleteNote={deleteNote}
+          />
+          {currentNoteId && notes.length > 0 && (
+            <Editor currentNote={findCurrentNote()} updateNote={updateNote} />
+          )}
+        </Split>
+      ) : (
+        <div className="no-notes">
+          <h1>You have no notes</h1>
+          <button className="first-note" onClick={createNewNote}>
+            Create one now
+          </button>
+        </div>
+      )}
+    </main>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './css/index.css';
+import 'react-mde/lib/styles/css/react-mde-all.css';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
